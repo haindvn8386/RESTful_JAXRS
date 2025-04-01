273e1b22905931e88c31dbd80fbc5a962279273d
Administrator <haindvn@gmail.com>
2025-04-01 23:41:59 +0700

    First commit

---
+++ src/main/java/restful/jaxrs/ResTfulJaxrsApplication.java
@@ -0,0 +1,13 @@
+package restful.jaxrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulJaxrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResTfulJaxrsApplication.class, args);
+    }
+
+}
